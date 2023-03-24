8b4ea4785a6661cf7922cf38b8aeb53e24f81841
Muhammadjon <muhammadjon141998@gmail.com>
2023-03-24 17:05:54 +0500

    Basics

---
+++ #1-lesson/index.js
@@ -0,0 +1,34 @@
+// Intro
+/*
+    JavaScript was invented by Brendan Each in 1995
+    Developed for Netscape 2, LiveScript
+    Became ECMA in 1997
+    What makes JavaScript unique?
+        Full integration with HTML/CSS
+        Simple things are done simply
+        Supported by all major browsers and enabled by default
+    JS compiled by
+        in html
+        node console
+        online js compilers
+*/
+
+// let text = 'Lorem ipsum set dolor amet ...';
+// console.log(text);
+
+// let num = 1, a = 2, b = 3;
+/*
+    Data types
+
+    Primitives:
+        Number
+        String 
+        Boolean
+        Null
+        Undefined
+        Symbol
+        BigInt
+
+    Non Primitives (Refference type):
+        Object
+*/
