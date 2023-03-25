79e318c11fc076bf1c90e9774ac08e138031db2d
Muhammadjon <muhammadjon141998@gmail.com>
2023-03-25 17:08:13 +0500

    while, do while, functions

---
+++ #5-lesson/index.js
@@ -0,0 +1,26 @@
+// let num = 3;
+
+// while(num) {
+//     console.log(num);
+//     n--;
+// }
+
+
+// let num2 = 0;
+// do{
+//     console.log(num2);
+//     n--;
+// } while(num2);
+
+// function greetings() {
+//     alert('Hi');
+// }
+
+// function expression can not be accessed before initialization
+// const greetings = function () {
+//     alert('Hi');
+// }
+
+const arrowFunc = () => {
+    console.log('this is an arrow function');
+}
